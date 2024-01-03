8ae7e64a2a642875c23c5c702315a97a28f924cb
Finn Smith <finnbarsmith@gmail.com>
2024-01-02 17:14:22 -0800

    Basic code for OpenSidebar.js

--- OpenSidebar.js
+++ OpenSidebar.js
@@ -1,3 +1,6 @@
-function myFunction() {
-  
+function openCurrencySidebar() {
+  var html = HtmlService.createHtmlOutputFromFile('Sidebar')
+      .setTitle('Currency Converter')
+      .setWidth(300);
+  SpreadsheetApp.getUi().showSidebar(html);
 }
