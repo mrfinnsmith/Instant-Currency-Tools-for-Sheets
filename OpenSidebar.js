0d728a389c202b729332e37b8d68b5b4068a1dc9
Finn Smith <finnbarsmith@gmail.com>
2024-01-02 19:20:49 -0800

    Add function to pull in currencies for dropdowns

--- OpenSidebar.js
+++ OpenSidebar.js
@@ -4,3 +4,11 @@ function openCurrencySidebar() {
       .setWidth(300);
   SpreadsheetApp.getUi().showSidebar(html);
 }
+
+// Function to fetch available currencies from Frankfurter API and return as JSON string
+function getCurrencies() {
+  var url = 'https://api.frankfurter.app/currencies';
+  var response = UrlFetchApp.fetch(url);
+  var currencies = JSON.parse(response.getContentText());
+  return currencies;
+}
