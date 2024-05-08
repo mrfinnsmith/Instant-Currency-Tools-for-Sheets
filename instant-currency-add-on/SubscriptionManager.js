964fa50058adc211a2fffe084d9415fdd218f886
Finn Smith <finnbarsmith@gmail.com>
2024-05-08 12:47:35 -0700

    Standardize script properties to UPPER-KEBAB-CASE

--- instant-currency-add-on/SubscriptionManager.js
+++ instant-currency-add-on/SubscriptionManager.js
@@ -4,11 +4,11 @@ var cache = CacheService.getScriptCache();
 function getMongoDBProperties() {
     var scriptProperties = PropertiesService.getScriptProperties();
     return {
-        apiKey: scriptProperties.getProperty('mongoDbApiKey'),
-        baseUrl: scriptProperties.getProperty('mongoDbBaseUrl'),
-        dbName: scriptProperties.getProperty('mongoDbDatabaseName'),
-        collectionName: 'subscriptions', // Collection name for subscriptions
-        clusterName: scriptProperties.getProperty('mongoDbClusterName') // Assuming similar property is set
+        apiKey: scriptProperties.getProperty('MONGO-API-KEY'),
+        baseUrl: scriptProperties.getProperty('MONGO-BASE-URL'),
+        dbName: scriptProperties.getProperty('MONGO-DB-NAME'),
+        collectionName: scriptProperties.getProperty('MONGO-SUBSCRIPTION-COLLECTION-NAME'),
+        clusterName: scriptProperties.getProperty('MONGO-CLUSTER-NAME')
     };
 }
 
