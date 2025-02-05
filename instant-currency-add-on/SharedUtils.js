57bed49994926eb5cbd98ac9bf52870974707984
Finn Smith <finnbarsmith@gmail.com>
2025-02-04 16:52:34 -0800

    Add storeRateInMongoDB to SharedUtils

--- instant-currency-add-on/SharedUtils.js
+++ instant-currency-add-on/SharedUtils.js
@@ -8,3 +8,31 @@ function getMongoDBProperties() {
       clusterName: scriptProperties.getProperty('MONGO-CLUSTER-NAME')
   };
 }
+
+function storeRateInMongoDB(from, to, rate, date) {
+  var props = getMongoDBProperties();
+  var updateUrl = props.baseUrl + "/action/updateOne";
+
+  var updatePayload = {
+      dataSource: props.clusterName,
+      database: props.dbName,
+      collection: props.collectionName,
+      filter: { "_id": "exchange_rates" },
+      update: { $set: { [`rates.${from}_${to}.${date}`]: { rate: rate, lastUpdated: new Date().toISOString() } } },
+      upsert: true
+  };
+
+  var options = {
+      method: "post",
+      contentType: "application/json",
+      headers: { "api-key": props.apiKey },
+      payload: JSON.stringify(updatePayload),
+      muteHttpExceptions: true
+  };
+
+  try {
+      UrlFetchApp.fetch(updateUrl, options);
+  } catch (error) {
+      console.error("Failed to update MongoDB:", error.toString());
+  }
+}
